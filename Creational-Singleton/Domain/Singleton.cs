86c4dcf2f13ab8775a4552f2d02dd532243e7ed5
ltavares <larissa.tavares@w3as.com.br>
2024-08-27 16:25:06 -0300

    Adds singletons pattern

---
+++ Creational-Singleton/Domain/Singleton.cs
@@ -0,0 +1,31 @@
+﻿namespace Creational_Singleton.Domain
+{
+    //Singleton seguro para threads
+    public sealed class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        private static readonly object _lock = new object();
+
+        public static Singleton GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                        _instance.Value = value;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Creational-Singleton/README.md
@@ -0,0 +1,4 @@
+﻿### Singleton
+#### Padrão de projeto CRIACIONAL
+
+* Permite garantir que uma classe tenha apenas uma instância, enquanto provê um ponto de acesso global para essa instância
